ec9d71289842ebf17567e8b4ea032595797d0995
Manu Lanikeu <emmanuel.arethens1@gmail.com>
2017-06-22 15:59:04 +0200

    ajout du header

---
+++ header.php
@@ -0,0 +1,84 @@
+
+<section id="header">
+<img src="images/logo2.png" alt="logolanikeu" id="logolanikeu"> </img>
+<h1> Lanikeu's Blog </h1>
+
+<section id="reseauxsociaux">
+        <a href="https://twitter.com/Manu_Lanikeu"> <img src="images/twitter.png" alt="twitterlogo" id="logotwitter"> </a>
+        <a href="https://www.facebook.com/Lanikeu/"> <img src="images/fb.png" alt="facebooklogo" id="logofb"> </a>
+        <a href="https://www.twitch.tv/lanikeu"> <img src="images/twitch.png" alt="twitchlogo" id="logotwitch"> </a>
+</section>
+
+
+</section>
+
+<style>
+
+@font-face {
+    font-family: 'bignoodletoo';
+    src: url('images/bignoodletoo.ttf');
+}
+
+@font-face {
+    font-family: 'koverwatch';
+    src: url('images/koverwatch.ttf');
+}
+
+
+h1 {
+    font-family:'koverwatch';
+    font-size:60px;
+    color:orange;
+}
+
+#header {
+    background-image:url(images/headerbackground.png);
+    display:flex;
+    justify-content:center;
+    justify-content:space-between;
+}
+
+#reseauxsociaux {
+    align-items: center;
+    display: flex;
+}
+
+
+#logolanikeu {
+    height:120px;
+    align-self: center;
+}
+
+#logofb {
+    height:60px;
+}
+
+#logotwitch {
+    height:60px;
+}
+
+#logotwitter {
+    height:60px;
+}
+
+#logofb:hover {
+    opacity:0.8;
+}
+
+#logotwitter:hover {
+    opacity:0.8;
+}
+
+#logotwitch:hover {
+    opacity:0.8;
+}
+
+a {
+    text-decoration:none;
+}
+
+a:focus {
+    outline:0;
+}
+
+</style>
