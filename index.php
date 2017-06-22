64ef9bd37c6a13cced984bfad8fee9da51d30e3b
Manu Lanikeu <emmanuel.arethens1@gmail.com>
2017-06-22 17:23:44 +0200

    mise en page

--- index.php
+++ index.php
@@ -10,10 +10,53 @@
 </head>
 
 <body>
-<header>
-<?php include_once "./header.php" ?>
-</header>
+    <header>
+         <?php include_once "./header.php" ?>
+    </header>
 
+    <section id="megasection">
+        <section id="navgauche">
+        <a href="index.php"> <h2> Accueil </h2></a>
+        <a href="live.php"> <h2> Live </h2> </a>
+        <h3> About me </h3>
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque harum, tempora id obcaecati at hic quis voluptate? Repudiandae
+        minus ex quae mollitia. Molestias ratione nesciunt, dolor debitis numquam excepturi repellat?</p>
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam asperiores veritatis a facere
+        tenetur minima beatae inventore omnis odio ipsam. Hic assumenda, b
+        eatae totam quos, ipsam numquam illo laboriosam eligendi. </p>
+        </section>
+
+            <section id="main">
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam asperiores veritatis a facere
+            tenetur minima beatae inventore omnis odio ipsam. Hic assumenda, b
+            eatae totam quos, ipsam numquam illo laboriosam eligendi. </p>
+
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam asperiores veritatis a facere
+             tenetur minima beatae inventore omnis odio ipsam. Hic assumenda, b
+             eatae totam quos, ipsam numquam illo laboriosam eligendi. </p>
+
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam asperiores veritatis a facere
+            tenetur minima beatae inventore omnis odio ipsam. Hic assumenda, b
+            eatae totam quos, ipsam numquam illo laboriosam eligendi. </p>
+
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam asperiores veritatis a facere
+            tenetur minima beatae inventore omnis odio ipsam. Hic assumenda, b
+            eatae totam quos, ipsam numquam illo laboriosam eligendi. </p>
+
+             </section>
+
+        <section id="navdroite">
+        <a href="index.php"> <h2> Accueil </h2></a>
+        <a href="live.php"> <h2> Live </h2> </a>
+        <h3> About me </h3>
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque harum, tempora id obcaecati at hic quis voluptate? Repudiandae
+        minus ex quae mollitia. Molestias ratione nesciunt, dolor debitis numquam excepturi repellat?</p>
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam asperiores veritatis a facere
+        tenetur minima beatae inventore omnis odio ipsam. Hic assumenda, b
+        eatae totam quos, ipsam numquam illo laboriosam eligendi. </p>
+        </section>
+    
+    </section>
 </body>
 
 </html>
